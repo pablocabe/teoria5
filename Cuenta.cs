bf3e8c6e0403211b0bc38e094b1f56f56ec95892
pablocabe <pablocabe0@gmail.com>
2025-04-11 13:17:51 -0300

    first commit

---
+++ Cuenta.cs
@@ -0,0 +1,9 @@
+namespace Teoria5;
+class Cuenta{
+    public int Monto;
+    public static int Total;
+    public static void ImprimirResumen(){
+        Console.WriteLine($"Total: = {Total}");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Teoria5;
+Cuenta.ImprimirResumen();
+Cuenta c0 = new Cuenta();
+// c0.ImprimirResumen(); // Hay que sacarle el static para que compile porque es una referencia
+
+ Cuenta c1 = new Cuenta();
+ Cuenta c2 = new Cuenta();
+ c1.Monto = 20;
+ Cuenta.Total += c1.Monto;
+ c2.Monto = 30;
+ Cuenta.Total += c2.Monto;
+ Cuenta.ImprimirResumen();
