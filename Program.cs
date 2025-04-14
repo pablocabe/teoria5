6eb25687cced82c9ccd3c6afc1630bc025862492
pablocabe <pablocabe0@gmail.com>
2025-04-14 20:31:52 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using Teoria5;
-Cuenta.ImprimirResumen();
-Cuenta c0 = new Cuenta();
-// c0.ImprimirResumen(); // Hay que sacarle el static para que compile porque es una referencia
 
- Cuenta c1 = new Cuenta();
- Cuenta c2 = new Cuenta();
- c1.Monto = 20;
- Cuenta.Total += c1.Monto;
- c2.Monto = 30;
- Cuenta.Total += c2.Monto;
- Cuenta.ImprimirResumen();
+Cuenta c1 = new Cuenta();
+c1.Depositar(100).Depositar(50).Extraer(120).Extraer(50);
+Cuenta c2 = new Cuenta();
+c2.Depositar(200).Depositar(800);
+new Cuenta().Depositar(20).Extraer(20);
+c2.Extraer(1000).Extraer(1);
+Console.WriteLine("\nDETALLE");
+Cuenta.ImprimirDetalle();
+
+/*
+Se creó la cuenta Id=1
+Se depositó 100 en la cuenta 1 (Saldo=100)
+Se depositó 50 en la cuenta 1 (Saldo=150)
+Se extrajo 120 de la cuenta 1 (Saldo=30)
+Operación denegada - Saldo insuficiente
+Se creó la cuenta Id=2
+Se depositó 200 en la cuenta 2 (Saldo=200)
+Se depositó 800 en la cuenta 2 (Saldo=1000)
+Se creó la cuenta Id=3
+Se depositó 20 en la cuenta 3 (Saldo=20)
+Se extrajo 20 de la cuenta 3 (Saldo=0)
+Se extrajo 1000 de la cuenta 2 (Saldo=0)
+Operación denegada - Saldo insuficiente
+DETALLE
+CUENTAS CREADAS: 3
+DEPÓSITOS: 5 - Total depositado: 1170
+EXTRACCIONES: 3 - Total extraído: 1140
+ - Saldo 30
+ * Se denegaron 2 extracciones por falta de fondos
+*/
